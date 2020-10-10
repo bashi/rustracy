6663755ec35132a13db3b59e1740bf1107d34627
Kenichi Ishibashi <ishibashi.kenichi@gmail.com>
2020-10-10 16:41:25 +0900

    tracy-sys: Fix build.rs

--- tracy-sys/build.rs
+++ tracy-sys/build.rs
@@ -1,20 +1,32 @@
-fn main() {
-    println!("cargo:rustc-link-search=/home/bashi/work/rustracy/lib");
+use std::env;
 
-    // TODO: Figure out how to build this library on unix platforms.
-    #[cfg(target_os = "windows")]
-    {
-        println!("cargo:rustc-link-lib=user32");
-        cc::Build::new()
-            .use_plt(false)
-            .flag_if_supported("-flto")
-            .flag_if_supported("/LTCG")
-            .flag_if_supported("/O2")
-            .cpp(true)
-            .include("./tracy")
-            .define("TRACY_ENABLE", None)
-            .define("NDEBUG", None)
-            .file("./tracy/TracyClient.cpp")
-            .compile("libtracy.a");
+fn configure_build(mut b: cc::Build) -> cc::Build {
+    if env::var("CARGO_FEATURE_ALLOW_VM").is_ok() {
+        b.define("TRACY_TIMER_QPC", None);
     }
+    b
+}
+
+fn main() {
+    let target_os = match env::var("CARGO_CFG_TARGET_OS") {
+        Ok(target_os) => target_os,
+        Err(err) => panic!("Can't detect target os: {:?}", err),
+    };
+
+    if target_os == "windows" {
+        println!("cargo:rustc-link-lib=user32");
+    }
+
+    let b = cc::Build::new();
+    configure_build(b)
+        .file("./tracy/TracyClient.cpp")
+        .opt_level(3)
+        .flag_if_supported("-Wno-class-memaccess")
+        .flag_if_supported("-Wno-enum-compare")
+        .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wno-unused-function")
+        .cpp(true)
+        .define("TRACY_ENABLE", None)
+        .define("NDEBUG", None)
+        .compile("libtracy.a");
 }
